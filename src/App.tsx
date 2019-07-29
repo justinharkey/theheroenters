9355cdc6a93c7e4f8fd9a07172c91d75aa741415
Justin Harkey <justin.harkey@gmail.com>
2019-07-29 14:10:31 -0700

    Initial code commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <div className="app__mainMenu">list of stuff</div>
+      <div className="app__content">content here</div>
+    </div>
+  );
+}
+
+export default App;
